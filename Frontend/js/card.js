14bfa6aae75bfae0dc863dbe2efc00a72bf20d00
Hassan-jr <abdiladifhassan115@gmail.com>
2023-03-02 14:12:13 +0300

    store card added

---
+++ Frontend/js/card.js
@@ -0,0 +1,23 @@
+// // This is just an example of how to add dynamic data to the shopping card
+// const productName = "Sneaker Shoes";
+// const productRating = 3;
+// const storeName = "Store Name";
+
+// const productImage = document.querySelector(".product-image");
+// const productNameElement = document.querySelector(".product-name");
+// const productRatingElement = document.querySelector(".product-rating");
+// const storeNameElement = document.querySelector(".store-name");
+
+// productNameElement.textContent = productName;
+
+// // Add the rating stars dynamically
+// for (let i = 1; i <= 5; i++) {
+//   const starElement = document.createElement("span");
+//   starElement.classList.add("fa", "fa-star");
+//   if (i <= productRating) {
+//     starElement.classList.add("checked");
+//   }
+//   productRatingElement.appendChild(starElement);
+// }
+
+// storeNameElement.textContent = storeName;
