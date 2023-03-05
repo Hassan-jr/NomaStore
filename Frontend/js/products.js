584f9cc32033815d84c81defa1b422235532c0d5
Hassan-jr <abdiladifhassan115@gmail.com>
2023-03-05 13:51:53 +0300

    dammy data added

--- Frontend/js/products.js
+++ Frontend/js/products.js
@@ -1,4 +1,4 @@
-
+import { data } from "./data.js";
 //   Products to be changed later
 const products = [
   {
@@ -28,8 +28,8 @@ const products = [
     price: "Ksh:450",
     image: "../assets/sneaker.jpg",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
-    stars_5: 10,
-    stars_4: 15,
+    stars_5: 35,
+    stars_4: 25,
     stars_3: 17,
     stars_2: 14,
     stars_1: 6,
@@ -129,13 +129,14 @@ const products = [
 // totalRatings_2: ((stars_5 + stars_4 + stars_3 + stars_2 + stars_1)),
 let rattings_2
 let totalRatings_2
+let pointsratting
 const rate = [1,2,3,4,5]
 // fuction to generate the html file
 function generateProductCard(product) {
   return `
     <div class="shopping-card">
       <a href="individualCard.html" class="card">
-        <img src="${product.image}" alt="Product Image" class="product-image">
+        <img src="${product.imageurl}" alt="Product Image" class="product-image">
         <div class="product-details">
           <p class="store-name">Sold by: <span class="store_name2">${product.storeName}</span></p>
           <h2 class="product-name">${product.name}</h2>
@@ -149,13 +150,15 @@ function generateProductCard(product) {
                ${
                 rattings_2 = ((5*product.stars_5) + (4*product.stars_4)+(3*product.stars_3) + (2*product.stars_2) + (1*product.stars_1))/(product.stars_5 + product.stars_4 + product.stars_3 + product.stars_2 + product.stars_1),
                 console.log("rattings", rattings_2),
+                pointsratting = Math.round(rattings_2),
                 rate.map(i=> {
                   if(i<=rattings_2)
                     return `<i class="material-icons ">star</i>`
-                  else if ( rattings_2 - i < 0)
-                    return `<i class="material-icons ">star_outline</i>`
-                  else
+                  else if (pointsratting - rattings_2 != 0 )
                     return `<i class="material-icons ">star_half</i>`
+                  else
+                    return `<i class="material-icons ">star_outline</i>`
+                 
                 }).join('')
                }
               <span class="product-rating-count">${product.stars_5 + product.stars_4 + product.stars_3 + product.stars_2 + product.stars_1}</span>
@@ -172,7 +175,7 @@ function generateProductCard(product) {
 function generateProductCards() {
   const productCardsContainer = document.getElementById('product-cards');
   let productCardsHTML = ''; // collects the html card to be displayed
-  for (const product of products) {
+  for (const product of data) {
     productCardsHTML += generateProductCard(product);
   }
   productCardsContainer.innerHTML = productCardsHTML;
