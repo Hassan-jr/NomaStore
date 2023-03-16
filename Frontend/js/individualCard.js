9a3752467aeef07381895a6c19ef58d09581f2cf
Hassan-jr <abdiladifhassan115@gmail.com>
2023-03-16 13:12:04 +0300

    cart added

--- Frontend/js/individualCard.js
+++ Frontend/js/individualCard.js
@@ -1,77 +1,87 @@
 import { data } from "./data.js";
-// get the id for required item from session storage
-let id = window.sessionStorage.getItem("ProductID");
+import { data2 } from "./data2.js";
 
-
-// get the data of the id
-// +++++++++++++++++++ to be removed later +++++++++++++++++++
-let cardData = data.find((item) => item.id == id);
-
-
-const images = [
-  "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71MzaFpWjwL._AC_UL320_.jpg",
-  "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71F29ae7awL._AC_SX425_.jpg",
-  "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71aAk1BMhpL._AC_UL320_.jpg",
-  "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61LFz4eGS6L._AC_UL320_.jpg",
-  "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61t4mpabO+L._AC_UL320_.jpg",
-  "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/616nC5r+gyL._AC_UL320_.jpg",
-];
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+console.log("finalyy", id);
 
 // get the individualcard div
 const individualcard = document.getElementById("individualCard");
-individualcard.innerHTML = `
-<div class="card">
-<!-- THE IMAGE SLIDE SIDE -->
-<div class="container">
 
-<!-- all images -->
- <div class="images">
-  ${images.map(
-    (imgitem) =>
-     ` 
-  <div class="mySlides">
-    <img class="slideImage" src=${imgitem} style="width:100%">
+async function getData() {
+  let cardData = await data2.find((item) => item._id == id);
+  console.log("THE DATA", cardData);
+
+  if (cardData) {
+    individualcard.innerHTML = `
+   <div class="card">
+   <!-- THE IMAGE SLIDE SIDE -->
+   <div class="container">
+   
+   <!-- all images -->
+    <div class="images">
+     ${cardData.images
+       .map(
+         (imgitem) =>
+           ` 
+     <div class="mySlides">
+       <img class="slideImage " src=${imgitem} style="width:100%">
+     </div>
+     `
+       )
+       .join("")}
+     </div>
+   
+     <!-- Next and previous buttons -->
+     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+     <a class="next" onclick="plusSlides(1)">&#10095;</a>
+   
+     <!-- Thumbnail images -->
+     <div class="row">
+     ${cardData.images
+       .map(
+         (imgitem, i) =>
+           `
+       <div class="column">
+         <img class="demo cursor" src=${imgitem} onclick="currentSlide(${
+             i + 1
+           })" alt="The Woods">
+       </div>
+       `
+       )
+       .join("")}
+     </div>
+   </div>
+   
+   <!-- ********************* THE TEXT SIDE ******************** -->
+   <div class="right">
+     <p class="lightColor" >Shop> ${cardData.seller} > <span class="bold">${cardData.brand}</span> </p>
+     <p class="inStock">IN STOCK</p>
+     <h1>Hassan's Chair</h1>
+     <h3 class="price">$ 80.26</h3>
+     <p class="lightColor">Supplement Right Metatarsal-Tarsal Joint with Nonautologous Tissue Substitute, Open Approach Supplement Right Metatarsal-Tarsal Joint with Nonautologous Tissue Substitute, Open Approach</p>
+     <div>
+     <span class="bold">Category: <span class="lightColor">Chair</span></span>
+     </div>
+     <hr/>
+     <!-- ************ COUNTER ********** -->
+     <span>
+     <span class="bold">QTY</span> <span class="counter">
+       <span class="down" onClick='decreaseCount(event, this)'>-</span>
+       <input id="qty" type="text" value="1">
+       <span class="up" onClick='increaseCount(event, this)'>+</span>
+   </span>
+   </span>
+  <!-- ************************ buttons ****************** -->
+  <div class="buttons">
+     <button class="btn">Buy</button>
+     <button class="btn">Add to Cart</button>
   </div>
-  `
-  ).join('')}
-  </div>
+   </div>
+   </div>
+   
+   `;
+  }
+}
 
-  <!-- Next and previous buttons -->
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
-
-  <!-- Thumbnail images -->
-  <div class="row">
-  ${images.map((imgitem, i)=>
-    `
-    <div class="column">
-      <img class="demo cursor" src=${imgitem} onclick="currentSlide(${i+1})" alt="The Woods">
-    </div>
-    `).join('')}
-  </div>
-</div>
-
-<!-- THE TEXT SIDE -->
-<div class="right">
-  <p class="lightColor" >Shop > Noma > <span class="bold">Hassan's Chair</span> </p>
-  <p class="inStock">IN STOCK</p>
-  <h1>Hassan's Chair</h1>
-  <h3 class="price">$ 80.26</h3>
-  <p class="lightColor">Supplement Right Metatarsal-Tarsal Joint with Nonautologous Tissue Substitute, Open Approach Supplement Right Metatarsal-Tarsal Joint with Nonautologous Tissue Substitute, Open Approach</p>
-  <div>
-  <span class="bold">Category: <span class="lightColor">Chair</span></span>
-  </div>
-  <hr/>
-  <!-- ************ COUNTER ********** -->
-  <span>
-  <span class="bold">QTY</span> <span class="counter">
-    <span class="down" onClick='decreaseCount(event, this)'>-</span>
-    <input id="qty" type="text" value="1">
-    <span class="up" onClick='increaseCount(event, this)'>+</span>
-</span>
-</span>
-</div>
-</div>
-
-`;
-
+getData()
--- Frontend/js/products.js
+++ Frontend/js/products.js
@@ -1,29 +1,33 @@
 import { data } from "./data.js";
+import {data2} from './data2.js'
 
 // variables for ratting functionalities
 let rattings_2
 let options
 const rate = [1,2,3,4,5]
 
+let id = 0
+
+
 // fuction to generate the html product card
 function generateProductCard(product) {
-  
   return `
-    <div class="shopping-card" id=${product.id} onclick="myFunction(${product.id})">
-      <a href="individualcard.html" class="card" >
-        <img src="${product.imageurl}" alt="Product Image" class="product-image" >
+    <div class="shopping-card" id=${product._id} onclick="handleCardClick(event)">
+      <a href="#" class="card" >
+        <img src="${product.images[0]}" alt="Product Image" class="product-image" >
         <div class="product-details">
-          <p class="store-name">Sold by: <span class="store_name2">${product.storeName}</span></p>
-          <h2 class="product-name">${product.name}</h2>
-          <p class="card_dec">${product.description}</p>
+          <p class="store-name">Sold by: <span class="store_name2">${product.seller}</span></p>
+          <p >${product.title}</p>
+         <!-- <p class="card_dec">${product.description}</p> -->
           <div class="line-break"></div>
           <div class="price_ratings">
             <div>
-              <h3>${product.price}</h3>
+              <h3>$ ${product.price}</h3>
             </div>
             <div class="product-rating">
                ${
-                rattings_2 = (((5*product.stars_5) + (4*product.stars_4)+(3*product.stars_3) + (2*product.stars_2) + (1*product.stars_1))/(product.stars_5 + product.stars_4 + product.stars_3 + product.stars_2 + product.stars_1)).toFixed(1),
+                rattings_2 = product.rating,
+                // (((5*product.stars_5) + (4*product.stars_4)+(3*product.stars_3) + (2*product.stars_2) + (1*product.stars_1))/(product.stars_5 + product.stars_4 + product.stars_3 + product.stars_2 + product.stars_1)).toFixed(1),
                 options=1,
                 rate.map(i=> {
                   if(i<=rattings_2)
@@ -38,7 +42,7 @@ function generateProductCard(product) {
                 }).join('')
                }
                <span class="product-rating-count">${rattings_2}</span>
-               <span class="product-rating-count">${product.stars_5 + product.stars_4 + product.stars_3 + product.stars_2 + product.stars_1}</span>
+               <span class="product-rating-count">${product.reviews}</span>
             </div>
           </div>
         </div>
@@ -48,12 +52,15 @@ function generateProductCard(product) {
 }
 
 
+
+
+
 // Loops throug the product array to create a product for each item
 function generateProductCards() {
   const productCardsContainer = document.getElementById('product-cards');
   let productCardsHTML = ''; // collects the html card to be displayed
  
-  for (const product of data) {
+  for (const product of data2) {
     productCardsHTML += generateProductCard(product);
   }
   productCardsContainer.innerHTML = productCardsHTML;
@@ -63,5 +70,6 @@ generateProductCards();
 
 
 
+console.log(id);
 
- 
+
