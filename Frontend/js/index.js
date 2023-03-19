b77aa733093400c240426a2954184f61ceb7bba7
Hassan-jr <abdiladifhassan115@gmail.com>
2023-03-19 12:38:54 +0300

    header added

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,18 +1,1 @@
-// import { get_nav } from "./nav.js";
 
-// // Get the nav bar
-// const result = get_nav();
-// console.log(result);
-// const nav = document.getElementById("nav");
-// nav.innerHTML = result;
-
-// //Responsive nav
-// const navToggle = document.querySelector(".nav-toggle");
-// const navLinks = document.querySelector(".nav-links");
-
-// navToggle.addEventListener("click", () => {
-//   navLinks.classList.toggle("toggle_nav");
-// });
-
-console.log(Math.round(0.5)); 
-console.log(Math.round(-0.5)); 
