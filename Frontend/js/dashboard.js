c48abde1b846def1636b165622961e99f7f7cd04
Hassan-jr <abdiladifhassan115@gmail.com>
2023-03-25 14:55:50 +0300

    dashboard edited2

--- Frontend/js/dashboard.js
+++ Frontend/js/dashboard.js
@@ -6,3 +6,22 @@ menuicn.addEventListener("click",()=>
 	nav2.classList.toggle("navclose");
 })
 
+// ORDERS
+const orders = document.getElementById("items")
+
+for(let i =0; i<10; i++){
+	orders.innerHTML +=`
+	<div  class="item1">
+	<div class="item-product">
+		<img src="https://m.media-amazon.com/images/I/41rR7Hswk4L._SL1500_.jpg">
+		<div>
+			<p class="item-product-title">Net10 Motorola Moto e 4G LTE Prepaid Smartphone</p>
+			<small>Price: $ 768.98</small><br>
+		</div>
+	</div>
+	<h3 class="t-op-nextlvl">2.9k</h3>
+	<h3 class="t-op-nextlvl">210</h3>
+	<h3 class="t-op-nextlvl label-tag">Active</h3>
+</div>
+	`
+}
