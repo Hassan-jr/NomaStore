a187fcdb8e987e51aa270071d0625e6963edd13a
Hassan-jr <abdiladifhassan115@gmail.com>
2023-04-01 14:32:03 +0300

    dashboard updated

--- Frontend/js/dashboard.js
+++ Frontend/js/dashboard.js
@@ -25,3 +25,57 @@ for(let i =0; i<5; i++){
 </div>
 	`
 }
+
+
+
+const dashboard = document.getElementById("dashboard")
+// ************************************ CHANGE Dashboard content **********************
+
+// dashboard page
+const dashboardPage = document.getElementById("option1")
+dashboardPage.addEventListener("click", ()=>{
+	dashboardPage.style.borderLeft = '5px solid #010058af'
+	dashboard.innerHTML 
+})
+
+// Home page
+const homePage = document.getElementById("option2")
+homePage.addEventListener("click", ()=>{
+	homePage.style.borderLeft = '5px solid #010058af'
+	dashboard.innerHTML = "HOME PAGE"
+})
+
+// Products page
+const productsPage = document.getElementById("option3")
+productsPage.addEventListener("click", ()=>{
+	productsPage.style.borderLeft = '5px solid #010058af'
+	dashboard.innerHTML = "productsPage PAGE"
+})
+
+// Orders
+const ordersPage = document.getElementById("option4")
+ordersPage.addEventListener("click", ()=>{
+	ordersPage.style.borderLeft = '5px solid #010058af'
+	dashboard.innerHTML = "ordersPage PAGE"
+})
+
+// SubscribersPage
+const subscribersPage = document.getElementById("option5")
+subscribersPage.addEventListener("click", ()=>{
+	subscribersPage.style.borderLeft = '5px solid #010058af'
+	dashboard.innerHTML = "subscribersPage PAGE"
+})
+
+// Settings page
+const settingsPage = document.getElementById("option6")
+settingsPage.addEventListener("click", ()=>{
+	settingsPage.style.borderLeft = '5px solid #010058af'
+	dashboard.innerHTML = "settingsPage PAGE"
+})
+
+// logoutPAGE
+const logoutPage = document.getElementById("logout")
+logoutPage.addEventListener("click", ()=>{
+	logoutPage.style.borderLeft = '5px solid #010058af'
+	dashboard.innerHTML = "logoutPage PAGE"
+})
